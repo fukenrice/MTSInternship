800ecf02061cc4e6ab99b47345db9d1b879e4602
fukenrice <63079551+fukenrice@users.noreply.github.com>
2023-05-29 20:48:51 +0300

    Add currency list screen

--- app/src/main/java/com/example/mtsinternship/viewmodel/CurrencyListViewModel.kt
+++ app/src/main/java/com/example/mtsinternship/viewmodel/CurrencyListViewModel.kt
@@ -1,4 +1,56 @@
 package com.example.mtsinternship.viewmodel
 
-class CurrencyListViewModel {
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mtsinternship.data.api.ApiService
+import com.example.mtsinternship.data.model.CurrencyModel
+import com.example.mtsinternship.utils.Resource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.json.JSONObject
+import javax.inject.Inject
+
+class CurrencyListViewModel @Inject constructor(private val apiService: ApiService) :
+    ViewModel() {
+    // TODO: Сделал адаптер, надо сделать вью модель, фрагмент + еще один фрагмент и вью модель. Попутно даггер + тесты
+    private val shownCurrencyList =
+        MutableLiveData<Resource<List<CurrencyModel>>>(Resource.loading(null))
+    private var currencyList: List<CurrencyModel> = listOf()
+
+    private val TAG = "CurrencyListViewModel"
+
+    @SuppressLint("CheckResult")
+    fun getCurrencies() {
+        shownCurrencyList.postValue(Resource.loading(null))
+        apiService.getCurrencies()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { response ->
+                    val newList = mutableListOf<CurrencyModel>()
+                    val rates = response.get("rates") as JSONObject
+                    rates.keys().forEachRemaining { key ->
+                        newList.add(CurrencyModel(key, rates.get(key).toString().toDouble()))
+                    }
+                    currencyList = newList.toMutableList()
+                    shownCurrencyList.postValue(Resource.success(newList.toMutableList()))
+                },
+                { error ->
+                    Log.d(TAG, "getCurrencies: " + error.message.toString())
+                    shownCurrencyList.postValue(Resource.error(error.message.toString(),
+                        shownCurrencyList.value?.data
+                    ))
+                }
+            )
+    }
+
+    fun getCurrencyList() = shownCurrencyList
+
+    fun filterList(query: String) {
+        shownCurrencyList.postValue(Resource.loading(shownCurrencyList.value?.data))
+        val filteredList = currencyList.filter { it.name.startsWith(query) }
+        shownCurrencyList.postValue(Resource.success(filteredList))
+    }
 }
